bc18814b3a2071ed13dc28a3c76a1041a3f78172
KALWAZACHRIS <150255204+KALWAZACHRIS@users.noreply.github.com>
2024-02-08 11:31:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function validation (password, email){
+    if (password == "2200706051@2024" || email == "cruzchris117@gmail.com" )
+    console.log(`User email is ${email}, login successful`)
+    else 
+    console.log(`incorrect user credentials`)
+    }
+    
+    validation("2200706051@2024","cruzchris117@gmailcom");
